d0519f7ff9560716828ba5138bfe8772b296b761
Bartek <bartek.lipinski.pl@gmail.com>
2021-01-05 18:53:48 +0100

    first comit

---
+++ Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Movement : NetworkBehaviour
+{
+    public Vector3 control;
+
+    public Color c;
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<NetworkIdentity>().hasAuthority)
+        {
+            GetComponent<Renderer>().material.color = c;
+
+            control = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+
+            GetComponent<PhysicsLink>().ApplyForce(control, ForceMode.VelocityChange);
+
+            if (Input.GetAxis("Cancel") == 1)
+            {
+                GetComponent<PhysicsLink>().CmdResetPose();
+            }
+        }
+    }
+}
